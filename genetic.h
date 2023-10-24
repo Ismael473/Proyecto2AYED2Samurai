a30401e979feb4ac76d9929ab7b5fb2d644e8a40
Specktic <111095859+Specktic@users.noreply.github.com>
2023-10-23 21:03:45 -0600

    Header basico para el algoritmo genetico

--- genetic.h
+++ genetic.h
@@ -1,11 +1,25 @@
 #ifndef GENETIC_H
 #define GENETIC_H
 
+#include <SamuraiA.h>
+#include <SamuraiB.h>
 
 class genetic
 {
 public:
-    genetic();
+    genetic();  // Constructor
+
+    //Calcula y guarda la aptitud del samurai
+    void setFitness(char s);
+
+    //Retorna la aptitud del samurai
+    int getFitness(char s);
+
+private:
+    int Fitness;    //Aptitud del samurai
+    SamuraiA SamA;  //Objeto SamuraiA
+    SamuraiB SamB;  //Objeto SamuraiB
+
 };
 
 #endif // GENETIC_H
