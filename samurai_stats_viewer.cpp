a63c929f406380b9dab7e4040ebfb773ae66d7bf
DixonJafet <48777523+JafetDS@users.noreply.github.com>
2023-10-25 17:11:27 -0600

    corrección clase Samurai

--- samurai_stats_viewer.cpp
+++ samurai_stats_viewer.cpp
@@ -1,12 +1,12 @@
-#include "ninja_stats_viewer.h"
+#include "samurai_stats_viewer.h"
 
-ninja_stats_viewer::ninja_stats_viewer(QObject *parent) : QObject(parent)
+samurai_stats_viewer::samurai_stats_viewer(QObject *parent) : QObject(parent)
 {
 
 }
 
 
-void ninja_stats_viewer::setTableWidget(QTableWidget* tableWidget){
+void samurai_stats_viewer::setTableWidget(QTableWidget* tableWidget){
 
     this->Table = tableWidget;
     this->Table->setEditTriggers(QAbstractItemView::NoEditTriggers);
@@ -15,7 +15,7 @@ void ninja_stats_viewer::setTableWidget(QTableWidget* tableWidget){
 }
 
 
-void ninja_stats_viewer::setNinjaStats(QString ID, QString Edad, QString Probabilidad,
+void samurai_stats_viewer::setSamuraiStats(QString ID, QString Edad, QString Probabilidad,
                                        QString Generaciones, QString Inteligencia,
                                        QString Condicion, QString Tronc_Sup,
                                        QString Tronc_Inf, QString Resistencia)
@@ -34,29 +34,16 @@ void ninja_stats_viewer::setNinjaStats(QString ID, QString Edad, QString Probabi
 
     }
 
-    /*
-    this->Table->setItem(row,IDC,new QTableWidgetItem(ID));
-    this->Table->setItem(row,EdadC,new QTableWidgetItem(Edad));
-    this->Table->setItem(row,ProbabilidadC,new QTableWidgetItem(Probabilidad));
-    this->Table->setItem(row,GeneracionesC,new QTableWidgetItem(Generaciones));
-    this->Table->setItem(row,InteligenciaC,new QTableWidgetItem(Inteligencia));
-    this->Table->setItem(row,CondicionC,new QTableWidgetItem(Condicion));
-    this->Table->setItem(row,Tronc_SupC,new QTableWidgetItem(Tronc_Sup));
-    this->Table->setItem(row,Tronc_InfC,new QTableWidgetItem(Tronc_Inf));
-    this->Table->setItem(row,ResistenciaC,new QTableWidgetItem(Resistencia));
-
-
-*/
 
 }
 
-void ninja_stats_viewer::insertToTable(QString elem){
+void samurai_stats_viewer::insertToTable(QString elem){
 
 
 }
 
 
-void ninja_stats_viewer::setStatsNames(){
+void samurai_stats_viewer::setStatsNames(){
 
     QStringList atributos;
     atributos<<"ID"<<"Edad"<<"Probabilidad"
